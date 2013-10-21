a9efefbcc59fb4f2cc19f8a5070f4b8094bcef3b
GloveDotCom <Jaren.G@Gmail.com>
2013-10-21 01:31:06 -0400

    removed junk & added services for distict year & make *flex*

--- api.py
+++ api.py
@@ -1,16 +1,41 @@
 from tastypie.resources import ModelResource
-#from tastypie.constants import ALL
+from tastypie.constants import ALL
 from fuel.models import Vehicle
+from django.core import serializers
+from tastypie import fields, utils
 
 class CarDataResource(ModelResource):
     class Meta:
+        model = Vehicle
+        fields = ('id','make')
         queryset = Vehicle.objects.all()
         resource_name = 'cardata'
         allowed_methods = ['get']
+        include_resource_uri = False
+        include_absolute_url = False
+
 
 
 class MakeResource(ModelResource):
+    make = fields.CharField(attribute='make')
+   # model=Vehicle
     class Meta:
-        queryset = Vehicle.objects.distinct('make')
+        queryset =Vehicle.objects.order_by('make').distinct('make')
         resource_name = 'make'
         allowed_methods = ['get']
+        #serializer = Serializer()
+        # list(queryset)
+        fields = ['make']
+        include_resource_uri = False
+        include_absolute_url = False
+        ordering = ['make']
+
+class YearResource(ModelResource):
+    class Meta:
+        queryset =Vehicle.objects.order_by('year').distinct('year')
+        resource_name = 'year'
+        allowed_methods = ['get']
+        fields = ['year']
+        include_resource_uri = False
+        include_absolute_url = False
+        ordering = ['year']
--- opendata/urls.py
+++ opendata/urls.py
@@ -1,23 +1,19 @@
 from django.conf.urls import patterns, include, url
-from api import CarDataResource
-from api import MakeResource
+from tastypie.api import Api
+from api import CarDataResource, MakeResource, YearResource
+from rest_framework import viewsets, routers
 from django.contrib import admin
 
 admin.autodiscover()
 
-#v1_api = Api(api_name='v1')
-#v1_api.register(CarDataResource())
+v1_api = Api(api_name='v1')
+v1_api.register(CarDataResource())
+v1_api.register(MakeResource())
+v1_api.register(YearResource())
 
-cardata_resource= CarDataResource()
-make_resource= MakeResource()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'opendata.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(cardata_resource.urls)),
-    url(r'^make/', include(make_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 
 )
