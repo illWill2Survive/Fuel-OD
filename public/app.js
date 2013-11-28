24d9ab91e7b0bc4a76deb4dc872926cba3583ac8
Daniel Carter <dl.carter@gmail.com>
2013-11-28 01:40:29 -0500

    require config changes.  controller currently not loading correctly

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
+/**
+* This file launches the routes, controllers, and templates
+*/
+
 define([
 	'angular',
-	'ngRoute'
+	'ngRoute',
+	'js/controllers/homectrl'
 ], function(angular) {
-	return angular.module('app', ['ngRoute'])
+	return angular.module('app', ['ngRoute', 'homeCtrl'])
 		.config(['$routeProvider', function($routeProvider) {
 			$routeProvider
-				.when('/', {controller: 'CHANGEME', templateUrl: '/CHANGEME'});
+				.when('/', {controller: 'HomeController', templateUrl: '/index.html'});
 		}]);
 });
