9b8220b5371ec72b05e4d52f563c7a6e73baf596
GloveDotcom <jaren.g@gmail.com>
2014-01-25 19:59:15 +0000

    updated gunicorn script

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 NAME="fuel_opendata"                                  # Name of the application
-DJANGODIR=/webapps/fuel_django/Fuel-OD/
-SOCKFILE=/webapps/fuel_django/Fuel-OD/run/gunicorn.sock  # we will communicte using this unix socket
+DJANGODIR=`pwd`
+SOCKFILE=${DJANGODIR}/run/gunicorn.conf  	# we will communicte using this unix socket
 USER=fuel                                        # the user to run as
 GROUP=webapps                                    # the group to run as
 NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=opendata.settings             # which settings file should Django use
 DJANGO_WSGI_MODULE=opendata.wsgi                     # WSGI module name
-LOGFILE=/webapps/fuel_django/Fuel-OD/log/fuel.log
-LOGDIR=$(dirname $LOGFILE)
+LOGFILE=${DJANGODIR}/log/fuel.log		#location of log file
+LOGDIR=$(dirname $LOGFILE)			#location of log directory 
+VIRENV=$DJANGODIR/../env			#you have to have the virtual env in the same director as your django dir
 
+#echo $VIRENV
+#cd ${VIRENV}
+#echo `pwd`
 echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-echo `pwd`
-source ../Fuel-OD/bin/activate
-export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+source ${VIRENV}/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE #set the django model 
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH  		#add django dir to python path
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
@@ -27,11 +30,11 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec ../Fuel-OD/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec ${VIRENV}/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
   --log-level=debug \
   --log-level=debug \
   --log-file=$LOGFILE \
-  --config=$SOCKFILE 2>> $LOGFILE  #bind=unix:$SOCKFILE
+  --config=$SOCKFILE 2>> $LOGFILE  
