a462fb75ae99fef616b0c02a4fc9a38f26d6d114
GloveDotcom <Jaren.G@Gmail.com>
2014-02-02 15:57:30 -0500

    dan lesson

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from fuel.models import Vehicle
 from django.core import serializers
 from tastypie import fields, utils
 
+
 dataset= Vehicle.objects.filter(fuelType1 ='Regular_Gasoline').filter(cylinders__lt=7).filter(year__gt=2011).filter(year__lt=2014).exclude(fuelCost08=0).exclude(highway08 =0).exclude(UCity =0).exclude(UHighway=0).exclude(youSaveSpend=0).exclude(fuelCostA08='0')
 
 
