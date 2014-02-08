39c77635c889cf99ef22b62ae8ac1e24960167ea
GloveDotcom <Jaren.G@Gmail.com>
2014-02-07 23:50:10 -0500

    remove API artifacts and updated scripts #26

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -11,7 +11,7 @@ DJANGO_WSGI_MODULE=opendata.wsgi                     # WSGI module name
 LOGFILE=${DJANGODIR}/log/fuel.log		#location of log file
 LOGDIR=$(dirname $LOGFILE)			#location of log directory 
 VIRENV=$DJANGODIR/../env			#you have to have the virtual env in the same director as your django dir
-
+API="tastypie"
 #echo $VIRENV
 #cd ${VIRENV}
 #echo `pwd`
@@ -19,6 +19,15 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
+if [ ! -d $API ]; then
+    echo "Type \"Y\" to auto download Tastypie artifacts , followed by [ENTER]:"
+    read command 
+
+    if [ "$command" == "Y" ] || [ "$command" == "y" ]; then
+        git clone https://github.com/toastdriven/django-tastypie
+    fi 
+fi
+
 source ${VIRENV}/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE #set the django model 
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH  		#add django dir to python path
