e8237f1bf748300441de7ad39b4b8df2da92759e
dcartertwo <dl.carter00@gmail.com>
2014-02-22 20:05:40 -0500

    added images to about page.  grunt config changes

--- app/app/aboutctrl.js
+++ app/app/aboutctrl.js
@@ -1,7 +1,7 @@
 require('angular/angular');
-require('./imagefact');
 
 exports.AboutController = function($scope, ImageFactory) {
-    $scope.whatsupdoc = "This app is dope";
-    console.log(ImageFactory.images);
+    $scope.images = ImageFactory.images;
+
+    $scope.test = 'testing';
 };
