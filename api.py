06d37a4b6b33d9769c4e35e7de1e7222b764f69a
GloveDotCom <Jaren.G@Gmail.com>
2014-03-02 08:22:52 +0000

    added proper filtering to api #62

--- api.py
+++ api.py
@@ -4,10 +4,6 @@ from fuel.models import Vehicle
 from django.core import serializers
 from tastypie import fields, utils
 
-#filter data but problem will change later
-#dataset= Vehicle.objects.filter(fuelType1 ='Regular_Gasoline').filter(cylinders__lt=7).filter(year__gt=2011).filter(year__lt=2014).exclude(fuelCost08=0).exclude(highway08 =0).exclude(UCity =0).exclude(UHighway=0).exclude(youSaveSpend=0).exclude(fuelCostA08='0')
-
-
 class HighwayMPGResource(ModelResource):
         class Meta:
             fields =["make", "model", "year", "UHighway"]
@@ -15,9 +11,9 @@ class HighwayMPGResource(ModelResource):
             resource_name= 'highway'
             allowed_methods = ['get']
             include_resource_uri = False
-            include_absolute_url = False
-            filtering ={'year': ALL}
             ordering = ['make']
+            filtering ={'year': ALL, "make":ALL, "model": ALL, "UHighway": ALL}
+		
 
 class CityMPGResource(ModelResource):
         class Meta:
@@ -27,8 +23,8 @@ class CityMPGResource(ModelResource):
             allowed_methods = ['get']
             include_resource_uri = False
             include_absolute_url = False
-            filtering ={'year': ALL}
             ordering = ['make']
+            filtering ={'year': ALL, "make":ALL, "model": ALL, "UCity" : ALL}
 
 class YouSaveSpendMPGResource(ModelResource):
         class Meta:
@@ -38,7 +34,7 @@ class YouSaveSpendMPGResource(ModelResource):
             allowed_methods = ['get']
             include_resource_uri = False
             include_absolute_url = False
-            filtering ={'year': ALL}
+            filtering ={'year': ALL, "make":ALL, "model": ALL, "youSaveSpend": ALL}
             ordering = ['model']
 
 class CarDataResource(ModelResource):
@@ -51,6 +47,7 @@ class CarDataResource(ModelResource):
         include_resource_uri = False
         include_absolute_url = False
         ordering = ['make']
+        filtering ={'year': ALL, "make":ALL, "model": ALL, "youSaveSpend": ALL}
 
 class MakeResource(ModelResource):
     class Meta:
@@ -61,14 +58,14 @@ class MakeResource(ModelResource):
         include_resource_uri = False
         include_absolute_url = False
         ordering = ['make']
+        filtering ={"make":ALL}
 
 class YearResource(ModelResource):
     class Meta:
         queryset = Vehicle.objects.all()
         resource_name = 'year'
         allowed_methods = ['get']
-        fields = ['year']
         include_resource_uri = False
         include_absolute_url = False
         ordering = ['year']
-
+        filtering ={'year': ALL}
--- opendata/settings.py
+++ opendata/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '=dz6@%2bju16vm=8$v-$mrmu19n&-w2#r8)ih$9@ug%4o07h=0y-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   # turn off once I push to AWS
@@ -25,7 +25,7 @@ TEMPLATE_DEBUG = False # ^^
 
 # When you go to proudction you need to add the host once DEBUG is set to false
 ALLOWED_HOSTS = [
-	'*' # in Production django restricts the host that can access your app 
+	'.jarenglover.com' # in Production django restricts the host that can access your app 
 ]
 
 
@@ -75,7 +75,30 @@ SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH = '/var/run/redis/redis.sock'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
+ 
+ 
+ 
+    'default': { 
+ 
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'. 
+ 
+            'NAME': 'fuel',                      # Or path to database file if using sqlite3. 
+ 
+            # The following settings are not used with sqlite3: 
+ 
+            'USER': 'illwill', 
+ 
+            'PASSWORD': '1D@t@$0ur30fGr3@tn3$$4', 
+ 
+            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP. 
+ 
+            'PORT': '5432',                      # Set to empty string for default. 
+ 
+        } 
+} 
+
+'''DATABASES = {
 
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -86,7 +109,7 @@ DATABASES = {
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '5432',                      # Set to empty string for default.
         }
-}
+}'''
 
 
 # Internationalization
