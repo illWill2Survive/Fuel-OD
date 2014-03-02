bbcb2bfbd30c14b61dce071ee112e20cdec8a8ab
GloveDotCom <Jaren.G@Gmail.com>
2014-03-02 08:46:03 +0000

    almost pushed settings file #62

--- opendata/settings.py
+++ opendata/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=dz6@%2bju16vm=8$v-$mrmu19n&-w2#r8)ih$9@ug%4o07h=0y-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   # turn off once I push to AWS
@@ -25,7 +25,7 @@ TEMPLATE_DEBUG = False # ^^
 
 # When you go to proudction you need to add the host once DEBUG is set to false
 ALLOWED_HOSTS = [
-	'.jarenglover.com' # in Production django restricts the host that can access your app 
+	'*' # in Production django restricts the host that can access your app 
 ]
 
 
@@ -75,30 +75,7 @@ SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH = '/var/run/redis/redis.sock'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = { 
- 
- 
- 
-    'default': { 
- 
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'. 
- 
-            'NAME': 'fuel',                      # Or path to database file if using sqlite3. 
- 
-            # The following settings are not used with sqlite3: 
- 
-            'USER': 'illwill', 
- 
-            'PASSWORD': '1D@t@$0ur30fGr3@tn3$$4', 
- 
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP. 
- 
-            'PORT': '5432',                      # Set to empty string for default. 
- 
-        } 
-} 
-
-'''DATABASES = {
+DATABASES = {
 
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -109,7 +86,7 @@ DATABASES = {
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '5432',                      # Set to empty string for default.
         }
-}'''
+}
 
 
 # Internationalization
