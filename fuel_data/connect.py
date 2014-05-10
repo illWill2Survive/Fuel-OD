3e7dfaae6111e003a7101c44b1569e5f33bc1246
GloveDotCom <Jaren.G@Gmail.com>
2014-05-10 04:57:04 +0000

    needful 3.0

--- fuel_data/connect.py
+++ fuel_data/connect.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 
+# I wrote this script to properly load data into my database
+# Author: Jaren Glover @GloveDotCom
+# 
+
 import sys, traceback
 import psycopg2  # make sure you have psycopg is installed -- use pip
 import csv      #pretty sure this is default in Python
 
-connection_string = ''
-                    
+connection_string = ""
+
+FILE = '' #name of the file you want to process  
+
 def main():
     try:
         print "trap or die"
         connect()
     except KeyboardInterrupt:
          print "Shutdown requested ... exiting"
-
     except Exception:
         traceback.print_exc(file=sys.stdout)
     sys.exit(0)
@@ -21,7 +26,7 @@ def main():
 def connect():
     try:
         conn= psycopg2.connect(connection_string)
-        global cur
+        global cur #made global so I can call it in the below process function
         cur = conn.cursor()
         process()
         conn.commit()
@@ -32,17 +37,13 @@ def connect():
 
 def process():
     try:
-        #IO = open("vehicles.csv")
-        #with open('vehicles_updated_NoHeader.csv','rb') as csvfile:
-        with open('noE.csv','rb') as csvfile:
+        with open(FILE,'rb') as csvfile:
             data = csv.reader(csvfile, delimiter='|', \
                     quoting=csv.QUOTE_MINIMAL)
             for row in data:
                 SQL= 'INSERT INTO fuel_vehicle (city08,"co2TailpipeGpm","comb08",cylinders,"drive","feScore","fuelCost08","fuelCostA08","fuelType1","highway08","highwayA08","car_id","make","model","trany", "UCity","UCityA","UHighway","UHighwayA","VClass","year","youSaveSpend") VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
                 #print cur.mogrify(SQL,row)
                 cur.execute(SQL,row)
-                
-       
 
     except Exception:
         traceback.print_exc(file=sys.stdout)
