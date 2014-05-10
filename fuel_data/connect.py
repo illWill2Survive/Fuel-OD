e96f4ee5aa2cb1b0ad2afcbb70281bc4c5608297
GloveDotCom <Jaren.G@Gmail.com>
2014-05-10 04:10:50 +0000

    rewrote needful

--- fuel_data/connect.py
+++ fuel_data/connect.py
@@ -10,7 +10,6 @@ def main():
     try:
         print "trap or die"
         connect()
-        process()
     except KeyboardInterrupt:
          print "Shutdown requested ... exiting"
 
@@ -24,42 +23,26 @@ def connect():
         conn= psycopg2.connect(connection_string)
         global cur
         cur = conn.cursor()
+        process()
+        conn.commit()
+        conn.close()
     except:
         print "I can't connect my G"
         traceback.print_exc(file=sys.stdout)
 
 def process():
     try:
-        #IO = open("vehicles_updated_NoHeader.csv")
+        #IO = open("vehicles.csv")
         #with open('vehicles_updated_NoHeader.csv','rb') as csvfile:
-        with open('fuel.csv','rb') as csvfile:
-            data = csv.reader(csvfile, delimiter='|', quotechar ='|', \
+        with open('noE.csv','rb') as csvfile:
+            data = csv.reader(csvfile, delimiter='|', \
                     quoting=csv.QUOTE_MINIMAL)
             for row in data:
-                SQL= 'INSERT INTO fuel_vehicle ("city08","co2TailpipeGpm","comb08","cylinders","drive","feScore","fuelCost08", "fuelCostA08", "fuelType1","highway08", "highwayA08","car_id", "make", "model","trany", "UCity", "UCityA", "UHighway", "UHighwayA", "VClass", "year","youSaveSpend") VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
-                #print cur.fetchall()
-                #print type(tuple(row))
-                temp=tuple(row)
-                #print type(temp)
-                #print temp
-                print SQL,row
-                print cur.mogrify(SQL,temp,)
-                #sys.exit(0)
-                print temp
-                #print cur.mogrify(SQL,(temp,))
-                #cur.execute(SQL,row)
+                SQL= 'INSERT INTO fuel_vehicle (city08,"co2TailpipeGpm","comb08",cylinders,"drive","feScore","fuelCost08","fuelCostA08","fuelType1","highway08","highwayA08","car_id","make","model","trany", "UCity","UCityA","UHighway","UHighwayA","VClass","year","youSaveSpend") VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+                #print cur.mogrify(SQL,row)
+                cur.execute(SQL,row)
                 
-            #conn.commit()
-            #conn.close()
        
-       
-       
-       
-       #CSVIO = csv.reader(IO)
-        
-        #for row in CSVIO:
-         #   if row[3] == ''or row[3] == 'NA':
-          #      row[3] = None
 
     except Exception:
         traceback.print_exc(file=sys.stdout)
